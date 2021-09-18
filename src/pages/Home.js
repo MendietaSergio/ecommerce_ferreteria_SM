c5218a91b284bfe0a88838bb6f9dc4929329cdd2
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-09-18 14:50:35 -0300

    Añade img portada en home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ItemListContainer from '../components/Container/ItemListContainer';
-
+import imgPort from "../images/portada.png"
 
 const Home = () =>{
     return (
         <>
+        <div className="mt-3 mb-3 " >
+          <img src={imgPort} title="Portada" width="100%"/>
+        </div>
           <ItemListContainer mensaje={"Productos"} />
         </>
     )
