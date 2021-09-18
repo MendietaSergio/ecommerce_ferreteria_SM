25e75abbbbef9b0eeb591560669b3bf9402085e0
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-09-18 14:49:47 -0300

    Añade redireccion por categoria

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,25 @@
 import ItemListContainer from "../components/Container/ItemListContainer";
+import { Link, useParams } from "react-router-dom";
 
-const Products = ( ) =>{
+const Products = ({product} ) =>{
+    const {idCategory} = useParams()
+    const {idSubCategory} = useParams()
     return (
         <>
-            <h1 className="text-center m-5">Productos</h1>
+        <h1 className="text-center m-5">{product}</h1>
+        <div>
+            <span className="link-page">
+                <Link to="/">Home<i class="fas fa-chevron-right"></i></Link>
+                {idCategory ?    
+                <Link to={`/productos`}>Productos<i class="fas fa-chevron-right"></i></Link>    
+                : null
+                }
+                {idCategory && idSubCategory ?    
+                <Link to={`/productos/${idCategory}`}>{idCategory}<i class="fas fa-chevron-right"></i></Link>    
+                : null
+                }
+            </span>
+        </div>
             <ItemListContainer/>
         </>
     )
