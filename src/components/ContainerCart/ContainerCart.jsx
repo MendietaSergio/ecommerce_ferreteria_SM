85207a99b2a139fec105d51cff9df9488cdcffcb
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-09-26 00:12:18 -0300

    Añade url a productos cuando el carrito esta vacio

--- src/components/ContainerCart/ContainerCart.jsx
+++ src/components/ContainerCart/ContainerCart.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { ItemCount } from "../Container/ItemCount";
 import Button from "../Button/Button";
 import "./ContainerCart.css";
 import { CartContextUse } from "../../Context/CartContext";
-
+import CartEmpty from "../../images/carritoVacio.png";
+import { Link } from "react-router-dom";
 const ContainerCart = () => {
   const { clear, removeItem, cart } = CartContextUse();
 
@@ -18,7 +18,14 @@ const ContainerCart = () => {
         </div>
         {cart.length === 0 ? (
           <>
-            <h1>No hay nada por aquí...</h1>
+            <div className="py-4">
+                <h1 className="text-center my-3">No hay nada por aquí...</h1>
+                <img
+                    className="card-img-top"
+                    src={CartEmpty}
+                    alt=""/>
+                <h3 className="text-center my-3">Puedes ver mas <Link className="products" to="/productos">productos</Link></h3>
+            </div>
           </>
         ) : (
           <>
@@ -58,23 +65,20 @@ const ContainerCart = () => {
                 </div>
               </>
             ))}
-                <div className="row">
-                  <div className="col">
-                    <h4 className="sub-amount">Subtotal: $123123</h4>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col d-flex justify-content-end mx-5 mb-5">
-                    <Button
-                      className="btn btn-danger mx-2"
-                      text="Cancelar todo"
-                    />
-                    <Button
-                      className="btn btn-success mx-2"
-                      text="Confirmar compra"
-                    />
-                  </div>
-                </div>
+            <div className="row">
+              <div className="col">
+                <h4 className="sub-amount">Subtotal: $123123</h4>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col d-flex justify-content-end mx-5 mb-5">
+                <Button className="btn btn-danger mx-2" text="Cancelar todo" />
+                <Button
+                  className="btn btn-success mx-2"
+                  text="Confirmar compra"
+                />
+              </div>
+            </div>
           </>
         )}
       </div>
