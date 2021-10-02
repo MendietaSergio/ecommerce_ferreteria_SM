1dc2007621ddeb712763bacb8916f0930eb44942
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-02 13:46:38 -0300

    Añade submit y change input al formulario

--- src/components/Contact/FormContact.jsx
+++ src/components/Contact/FormContact.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button"
 import "./FormContact.css"
 const FormContact =() =>{
+    const [ formData, setFormData ] = useState(initialData)
+    
+
+    const handleOnChange = (e) =>{
+        setFormData({
+          ...formData, //primero seteo lo que ya tiene
+          [e.target.name]:e.target.value//se guarda el valor del nombre de cada input
+         })
+         console.log(formData);
+      }
+    const handleOnSubmit = (e) =>{
+        e.preventDefault()
+        console.log("Enviado: ",formData);
+    }
     // agregar value a los input
     return(
         <>
@@ -9,32 +23,36 @@ const FormContact =() =>{
                 <h6 className="text-center">Formulario</h6>
             </div>
             <div className="form-col">
-                <form  >
+                <form onChange={handleOnChange} onClick={handleOnSubmit} >
                     <div className="form-group">
                         <label >Nombre completo: </label>
-                        <input className="form-control" type="text" />
+                        <input className="form-control" name="name" type="text" />
                     </div>
                     <div className="form-group">
                         <label >Correo: </label>
-                        <input className="form-control" type="text" />
+                        <input className="form-control" name="email" type="text" />
                     </div>
                     <div className="form-group">
                         <label >Asunto: </label>
-                        <input className="form-control" type="text" />
+                        <input className="form-control" name="affair" type="text" />
                     </div>
                     <div className="form-group">
                         <label >Comentario: </label>
-                        <textarea className="form-control" />
+                        <textarea className="form-control" name="comment"/>
                     </div>
-                    <div className="form-btn">
-                        <Button/>                    
+                    <div className="form-btn text-center">
+                        <Button onClick={handleOnSubmit} />
                     </div>
                 </form>
             </div>
-        
-        
         </>
     )
+}
+const initialData = {
+    name: "",
+    email: "",
+    affair: "",
+    comment: "",
 }
 
 export default FormContact;
