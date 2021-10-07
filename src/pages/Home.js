4a953864060f7f966da86145f41aafd586807269
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-07 18:22:25 -0300

    Añade slider de productos en Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ItemListContainer from '../components/Container/ItemListContainer';
 import imgPort from "../images/portada.png"
+import Slider from "../components/Slider/Slider"
 
 const Home = () =>{
     return (
@@ -8,7 +9,7 @@ const Home = () =>{
         <div className="mt-3 mb-3 " >
           <img src={imgPort} title="Portada" alt="Portada" width="100%"/>
         </div>
-          <ItemListContainer mensaje={"Productos"} />
+          <Slider title="Productos"/>
         </>
     )
 
