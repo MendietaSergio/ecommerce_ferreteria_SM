70c93a9dbd70e1fd362ad8a70d15c9959a235244
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-08 14:51:59 -0300

    Añade mejoras en item

--- src/components/Container/Item.jsx
+++ src/components/Container/Item.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState} from "react";
 import Button from "../Button/Button"
 import { Link } from "react-router-dom";
 import "./Item.css"
+import { CartContextUse } from "../../Context/CartContext";
 
-const Item = ({product}) =>{
-   
+
+const Item = ({
+    product,
+    }) =>{
+    const [ cambiarBtn, setCambiarBtn ] = useState (true)
+    const {addItem} = CartContextUse();
+
+    const onAdd = () => {
+        addItem(product, 1)
+        console.log("producto agregado, ", product);
+        setCambiarBtn(false);
+    };
     return(
         <>
             <div key={product.id} className="card mt-2" >
-                <Link className="card-footer-detail" to={`/detalle/${product.id}`}>
+                <Link className="card-link-detail" to={`/detalle/${product.id}`}>
                     <img className="card-img-top" src={`/assets/img/Productos/${product.pictureUrl}`} alt={product.title}/>
                     <div className="card-body">
                         <h5 className="card-title">{product.title}</h5>
@@ -17,7 +28,11 @@ const Item = ({product}) =>{
                 <div className="card-footer ">
                     <div className="row">
                     <span className="price text-center">Precio ${product.price},00</span>
-                    <Button className="d-block btn btn-primary" text="Agregar al carrito"></Button>
+                    {cambiarBtn ? (
+                        <Button className="d-block btn btn-primary" text="Agregar al carrito" onClick={()=>onAdd()} />
+                    ) : (
+                        <Button to="/carrito" text="Terminar compra" />
+                    )}
                     </div>
                 </div>
            </div>
