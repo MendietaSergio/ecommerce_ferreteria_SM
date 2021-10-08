2f4887ce7a0663c1c42b17cfa596aacb6e8583a4
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-08 17:33:32 -0300

    Añade slider en pagina de detalle del producto

--- src/components/Container/ItemDetail/ItemDetail.jsx
+++ src/components/Container/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import { ItemCount } from "../ItemCount/ItemCount";
+import Detail from "./Detail";
 import "./ItemDetail.css"
 
 const ItemDetail =({detail}) =>{
@@ -18,32 +19,7 @@ const ItemDetail =({detail}) =>{
                 <Link to={`/productos/${detail.category}/${detail.subCategory}`}>{detail.subCategory}</Link>
                 </span>
             <div key={detail.id} className="row  mt-2" >
-                <div className="col-lg-6 mb-4">
-                    <div className="main-product-image">
-                        <img className="main-product-image" src={`/assets/img/productos/${detail.pictureUrl}`} alt={detail.title}/>
-                    </div>
-                </div>
-                <div className="col-lg-6 p-5 ">
-                    <div className="detail-title">
-                    <p >Precio ${detail.price},00 por unid.</p>
-                    </div>
-                    <div className="detail-title">
-                        <div className="row">
-                            <div className="cart d-flex justift-content-center ">
-                                <ItemCount detail={detail} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="detail-title">
-                    <p>Categoría: {detail.category}</p>
-                    </div>
-                    <div className="detail-title">
-                    <p>Subcategoría: {detail.subCategory}</p>
-                    </div>
-                    <div className="detail-title">
-                    <p>Descripción: {detail.description}</p>
-                    </div>
-                </div>
+                <Detail detail={detail}/>
            </div>
         </>
     )
