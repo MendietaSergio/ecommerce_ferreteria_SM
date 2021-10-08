982fb41a410c2bfdd701d9e421e079dcfe2e020e
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-08 17:34:02 -0300

    Añade redireccion a slider destacados

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () =>{
           <img src={imgPort} title="Portada" alt="Portada" width="100%"/>
         </div>
         <div>
-          <Slider title="Destacados" productFeatured={true}/>
+          <Slider title="Destacados" productFeatured={true} to="/productos"/>
           <div className="d-flex justify-content-center my-5" >
             <Button to="/productos" text="Ver más"/>  
           </div>
@@ -18,7 +18,7 @@ const Home = () =>{
           <h3 className="text-center">Aprovechas nuestros productos con las mejores ofertas del día</h3>
         </div>
         <div>
-          <Slider title="Ofertas" productOffer={true}/>
+          <Slider title="Ofertas" productOffer={true} />
           <div className="d-flex justify-content-center my-5">
           </div>
         </div>
