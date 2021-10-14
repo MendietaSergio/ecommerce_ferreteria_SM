fbf2d4d8601672f1bf109580b53d6173177f355b
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-14 19:53:31 -0300

    Añade pagina para agregar productos

---
+++ src/pages/AdminAddProduct.js
@@ -0,0 +1,14 @@
+import AddProduct from "../components/AddProduct/AddProduct"
+
+const AdminAddProduct = ( ) =>{
+    return (
+        <>
+            <div className="container-body-pages">
+                <AddProduct/> 
+            </div>
+        </>
+    )
+
+}
+
+export default AdminAddProduct
