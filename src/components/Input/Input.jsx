41d4c7469d42556c128ba85f80dae5c4cb64a674
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-14 19:54:08 -0300

    Añade componente input, falta pasar el codigo del componente addproducts

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,36 @@
+import "./Input.css"
+
+const Input =({
+    type,
+    id,
+    name,
+    placeholder,
+    label,
+    defaultValue,
+    disabled,
+    register,
+    validation,
+    error
+}) =>{
+    return(
+        <>
+            <label className ="form-label">
+                {label && label} {/* condicionales */}
+                <input type={type}
+                name={name}
+                id={id}
+                disabled={disabled}
+                placeholder={placeholder}
+                defaultValue={defaultValue}
+                className={`form-control ${error ? 'input-error' : ''}`}
+                {...register(name,validation)}
+                autoComplete="off"
+                autoCorrect="off"
+                />            
+            </label>
+            {error ? <p className="text-danger">{error.message}</p> : null}
+        </>
+    )
+}
+
+export default Input;
