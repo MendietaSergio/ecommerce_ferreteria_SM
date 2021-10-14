8a4981dc47addb74d84b87cf055a92a902a54807
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-14 20:33:03 -0300

    Añade carpeta de validacion para agregar productos

---
+++ src/validations/AddProduct.js
@@ -0,0 +1,75 @@
+const validations = {
+    nameProduct:{
+      required :{
+        value: true,
+        message: "El campo es requerido."
+      },
+      minLength:{
+        value: 4,
+        message:"El nombre debe tener al menos 4 carácteres."
+      },
+      maxLength:{
+        value: 40,
+        message:"El nombre debe tener al máximo 40 carácteres."
+      }
+    },
+    category:{
+      required: {
+        value: true,
+        message: "El campo es requerido."
+      }
+    },
+    subCategory: {
+      required: {
+        value: true,
+        message: "El campo es requerido."
+      }
+    },
+    price:{
+      required:{
+        value:true,
+        message: "El campo es requerido."
+      },
+      min:{
+        value: 50,
+        message: "El valor mínimo es de $50"
+      }
+    },
+    offer:{
+      required:{
+        value:true,
+        message: "El campo es requerido."
+      }
+    },
+    discount:{
+      required:{
+        value:true,
+        message: "El campo es requerido."
+      },
+      min:{
+        value: 0,
+        message: "El descuento no puede ser un número negativo."
+      },
+      max: {
+        value: 40,
+        message:"El descuento máximo es de 40%"
+      }
+    },
+    stock:{
+      required:{
+        value:true,
+        message: "El campo es requerido."
+      },
+      min:{
+        value: 10,
+        message: "El stock minimo es de 10"
+      }
+    },
+    picture: {
+      required:{
+        value: true,
+        message:"El archivo es requerido."
+      },
+    }
+  }
+export default validations;
