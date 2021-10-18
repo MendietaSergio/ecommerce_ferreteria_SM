0e27e3b8a3a2d8e130c4ff807e510b78c8a03c26
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-18 15:45:09 -0300

    añade pagina de compra finalizada

---
+++ src/pages/PurchaseFinished.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PurchaseCompleted from "../components/PurchaseCompleted/PurchaseCompleted";
+
+const PurchaseFinished = () => {
+  return (
+    <>
+      <div className="container-body-pages">
+        <PurchaseCompleted />
+      </div>
+    </>
+  );
+};
+export default PurchaseFinished;
