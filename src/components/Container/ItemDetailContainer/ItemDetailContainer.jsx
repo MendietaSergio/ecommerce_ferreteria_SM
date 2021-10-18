ff071acbcf9f9c3ff8deee799f7d5a34249ee899
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-18 12:16:00 -0300

    Añade mensaje de error al no encontrar el detalle del producto seleccionado

--- src/components/Container/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/Container/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,41 +1,54 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
 import { getFirestore } from "../../../services/getFirebase";
 
-
-
-const ItemDetailContainer = ( ) =>{
-    const {idProduct} = useParams();
-    const [loading, setLoading] = useState(true)
-    const [detailProduct, setDetailProduct] = useState({})
-    useEffect(() =>{
-        const dbQuery = getFirestore()
-        const filterProduct = () =>{
-            if(idProduct){
-                dbQuery.collection('items').doc(idProduct).get()//Traigo el producto desde el id.
-                .then(resp =>{
-                    setDetailProduct({id: resp.id, ...resp.data()})//Seteo el nuevo array.
-                })
-                .catch(err => console.log("ERROR => ", err))
-                .finally(()=> setLoading(false))
+const ItemDetailContainer = () => {
+  const { idProduct } = useParams();
+  const [detailProduct, setDetailProduct] = useState({});
+  const [showDetailFail, setShowDetailFail] = useState(true);
+  const [message, setMessage] = useState('')
+  useEffect(() => {
+    const dbQuery = getFirestore();
+    const filterProduct = () => {
+      if (idProduct) {
+        dbQuery
+          .collection("items")
+          .doc(idProduct)
+          .get() //Traigo el producto desde el id.
+          .then((resp) => {
+            if (resp.data() != null) {
+              console.log("Producto existente, ", resp.data());
+              setDetailProduct({ id: resp.id, ...resp.data() }); //Seteo el nuevo array.
+              setShowDetailFail(false);
+            } else {
+              console.log("el producto no existe ", resp.data());
+              setShowDetailFail(true);
+              messageTime()
             }
-        }
-        filterProduct()
-    },[idProduct])
-
-    return (
-        <>
-        {loading ?
-        (
-            <div className="detail-loading">        
-            <h2 className="text-center my-4">Cargando <i className="fas fa-spinner fa-pulse" /></h2>
-            </div>
-        ):(
-            <ItemDetail detail={detailProduct}/>
-        )}
-        </>
-    )
-}
+          })
+          .catch((err) => console.log("ERROR => ", err));
+      }
+    };
+    filterProduct();
+  }, [idProduct]);
+  
+  const messageTime = ( ) =>{
+    setTimeout((setMessage("El producto no existe")),4000)
+  }
+  return (
+    <>
+      {showDetailFail ? (
+        <div className="detail-loading">
+          <h2 className="text-center my-4">
+            {message}
+          </h2>
+        </div>
+      ) : (
+        <ItemDetail detail={detailProduct} />
+      )}
+    </>
+  );
+};
 
 export default ItemDetailContainer;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import ItemDetailContainer from "../components/Container/ItemDetailContainer/ItemDetailContainer";
 
-
-
-const Detail =( )=>{
-        return(
-            <>
-            <div className="container-body-detail">
-                <ItemDetailContainer/>
-            </div>
-            </>
-        )
-} 
+const Detail = () => {
+  const [loading, setLoading] = useState(true);
+  setTimeout(()=> setLoading(false), 3000 )
+  return (
+    <>
+      {loading ? (
+        <div className="detail-loading">
+          <h2 className="text-center my-4">
+            Cargando <i className="fas fa-spinner fa-pulse" />
+          </h2>
+        </div>
+      ) : (
+        <div className="container-body-detail">
+          <ItemDetailContainer />
+        </div>
+      )}
+    </>
+  );
+};
 export default Detail;
