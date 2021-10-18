eee7d76dc45914b1dce0faa375cf89761cf6a60b
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-18 15:47:02 -0300

    Añade vista de de titulo y orden de compra o titulo que no tiene orden de compra

--- src/components/PurchaseCompleted/PurchaseCompleted.jsx
+++ src/components/PurchaseCompleted/PurchaseCompleted.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CartContextUse } from "../../Context/CartContext";
 
-const PurchaseCompleted = ({ completed }) => {
+const PurchaseCompleted = () => {
+  const { orderId } = CartContextUse();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [orderId]);
+
   return (
     <>
       <div className="container-body-pages">
-        <h1 className="text-center my-5">compra finalizada</h1>
+        {loading ? (
+          <h1 className="text-center my-5">
+            Cargando... <i className="fas fa-spinner fa-pulse" />
+          </h1>
+        ) : orderId === undefined ? (
+          <h1 className="text-center my-5">
+            Usted no tiene una orden de compra
+          </h1>
+        ) : (
+          <div>
+            <h1 className="text-center my-5">Compra finalizada !!</h1>
+            <h3 className="text-center my-5">Número de orden: {orderId}</h3>
+          </div>
+        )}
       </div>
     </>
   );
