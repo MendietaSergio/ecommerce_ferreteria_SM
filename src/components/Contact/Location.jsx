8f2f256c69d3de50a875291d793f6e2376b21857
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-20 00:45:51 -0300

    añade limpieza de error

--- src/components/Contact/Location.jsx
+++ src/components/Contact/Location.jsx
@@ -23,7 +23,7 @@ const Location = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52467.91573466545!2d-58.28173934716688!3d-34.724225043954796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32e38e45f5777%3A0x6a86dac21334524f!2sQuilmes%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1631823303123!5m2!1ses-419!2sar"
             width="100%"
             height="250"
-            allowfullscreen=""
+            
             loading="lazy"
             title="Ubicación"
           ></iframe>
